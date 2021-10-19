f18518831f509827907a5f2cbdd1fb7a8c453566
miku <svartkatt39@gmail.com>
2021-10-20 00:41:32 +0900

    change app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def main():
         u'born': 1815
     }) 
 
-    name = "Hoge"
+    name = "Hoge miku"
     #return name
     return render_template('hello.html', title='flask test', name=name) 
 
