ec6c7fa093b675c96e7aee13056e985f21cb8787
miku <svartkatt39@gmail.com>
2021-10-23 17:50:37 +0900

    firebase auth works

--- app.py
+++ app.py
@@ -4,76 +4,133 @@ from firebase_admin import firestore, auth
 
 from flask import Flask, request, jsonify, render_template, redirect, url_for, session
 import pyrebase
+import configparser
+import requests
 
 import json, os
-import config
+import cred
 
 # email = 'test001@example.com'
 # password = 'hogehogehoge'
 
 app = Flask(__name__)
 
-app.secret_key = config.SECRET_KEY
+app.secret_key = cred.SECRET_KEY
 
 # ===================== Firebase =====================================
 
 # Firebase初期化
-cred = credentials.Certificate({
-    "type":config.FIREBASE_TYPE,
-    "project_id": config.FIREBASE_PROJECT_ID,
-    "private_key": config.FIREBASE_PRIVATE_KEY.replace("\\n", "\n"),
-    "client_email": config.FIREBASE_CLIENT_EMAIL,
-    "token_uri": config.FIREBASE_TOKEN_URI
+creds = credentials.Certificate({
+    "type":cred.FIREBASE_TYPE,
+    "project_id": cred.FIREBASE_PROJECT_ID,
+    "private_key": cred.FIREBASE_PRIVATE_KEY.replace("\\n", "\n"),
+    "client_email": cred.FIREBASE_CLIENT_EMAIL,
+    "token_uri": cred.FIREBASE_TOKEN_URI
 })
-firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(creds)
 db = firestore.client()
-
-# with open("firebaseConfig.json") as f:
-#     firebaseConfig = json.loads(f.read())
-# firebase = pyrebase.initialize_app(firebaseConfig)
-# auth = firebase.auth()
 
 
 # ====================================================================
 
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
-# doc_ref = db.collection(u'users').document(u'alovelace')
-# doc_ref.set({
-#     u'first': u'MMMMY',
-#     u'last': u'Lovelace',
-#     u'born': 1815
-# }) 
+doc_ref = db.collection(u'users').document(u'alovelace')
+doc_ref.set({
+    u'first': u'MMMEY',
+    u'last': u'Lovelace',
+    u'born': 1815
+}) 
 
 
-# @app.route('/lp', methods=['GET'])
-# def lp():
-#     return render_template("lp.html")
+@app.route('/lp', methods=['GET'])
+def lp():
+    return render_template("lp.html")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template("login.html",msg="")
 
+    api_key = cred.FIREBASE_TOKEN_API_KEY
+
+    config = configparser.ConfigParser()
+    config.read("./config.ini")
+
+
     email = request.form['email']
     password = request.form['password']
+
+
     try:
-        user = auth.sign_in_with_email_and_password(email, password)
+        user = sign_in_with_email_and_password(api_key, email, password, config)
+        # user = auth.get_user_by_email(email)
+
         session['usr'] = email
+
+        print('---- sign_in_with_email_and_password -----')
+        print_pretty(user)
         return redirect(url_for('index'))
+
     except:
         return render_template("login.html", msg="メールアドレスまたはパスワードが間違っています。")
+
+def sign_in_with_email_and_password(api_key, email, password, config):
+
+    uri = f"https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key={api_key}"
+    headers = {"Content-type": "application/json"}
+    data = json.dumps({"email": email, "password": password, "returnSecureToken": True})
+    # config["proxy"]["proxy"] = True
+    proxies, verify = get_proxy(config)
+
+    print("proxies", proxies)
+    result = requests.post(url=uri,
+                           headers=headers,
+                           data=data,
+                           proxies=proxies,
+                           verify=verify)
+    print("result")
+    return result.json()
+
+def get_proxy(config):
+
+    verify = True
+    proxies = None
+
+    print("veryfy", verify)
+    print("proxies", proxies)
+    print("proxy", config["proxy"].getboolean("proxy"))
+
+    if config["proxy"].getboolean("proxy"):
+        proxies = {
+            "http": config["proxy"]["http"],
+            "https": config["proxy"]["https"]
+        }
+        verify = False
+
+    return proxies, verify
+
+def print_pretty(obj):
+    print(json.dumps(obj, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': ')))
+
 
 @app.route("/", methods=['GET'])
 def index():
     usr = session.get('usr')
     if usr == None:
-        return redirect(url_for('login'))
+        return redirect(url_for('lp'))
     return render_template("index.html", usr=usr)
 
 @app.route('/logout')
 def logout():
     del session['usr']
-    return redirect(url_for('login'))
+    return redirect(url_for('lp'))
 
 
 if __name__ == "__main__":
--- cred.py
+++ cred.py
@@ -2,10 +2,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os
+
 SECRET_KEY = os.getenv('sec_key')
 FIREBASE_TYPE=os.getenv('firebase_type')
 FIREBASE_PROJECT_ID = os.getenv('firebase_project_id')
 FIREBASE_PRIVATE_KEY = os.getenv('firebase_private_key')
 FIREBASE_CLIENT_EMAIL = os.getenv('firebase_client_email')
 FIREBASE_TOKEN_URI = os.getenv('firebase_token_uri')
+
+FIREBASE_TOKEN_API_KEY = os.getenv('firebase_api_key')
+
 
